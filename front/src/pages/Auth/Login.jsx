dcde048905cfebe391ec19acbb40a0219d22fc2f
amagnies <adrien.magnies@hotmail.fr>
2023-10-10 11:36:10 +0200

    Modification du swagger et fonctionnalité remember me local storage

--- front/src/pages/Auth/Login.jsx
+++ front/src/pages/Auth/Login.jsx
@@ -6,10 +6,9 @@ import auth_service from "@/_services/account.service";
 import { useSelector, useDispatch } from 'react-redux'
 
 const Login = () => {
-
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [rememberMe, setRememberMe] =  useState(false);
+    const [email, setEmail] = useState(localStorage.getItem('email') || '');
+    const [password, setPassword] = useState(localStorage.getItem('password') || '');
+    const [rememberMe, setRememberMe] = useState(localStorage.getItem('rememberMe') === 'true' || false);
     
     const dispatch = useDispatch();
     const navigate = useNavigate(); 
@@ -19,6 +18,19 @@ const Login = () => {
     
     const submitForm = (e) => {
         e.preventDefault();
+
+        if (rememberMe) {
+            // Sauvegardez l'e-mail, le mot de passe et la valeur "rememberMe" dans le localStorage
+            localStorage.setItem('email', email);
+            localStorage.setItem('password', password);
+            localStorage.setItem('rememberMe', rememberMe);
+        } else {
+            // Si l'utilisateur ne veut pas garder ses informations stockées, supprimez les valeurs du localStorage
+            localStorage.removeItem('email');
+            localStorage.removeItem('password');
+            localStorage.removeItem('rememberMe');
+        }
+
         dispatch(auth_service.login(email, password, rememberMe));
     }
 
@@ -27,7 +39,7 @@ const Login = () => {
         navigate('/User')
         }
     },[token, navigate])
-    
+
     return (
         <>
         <section className="sign-in-content">
@@ -36,14 +48,14 @@ const Login = () => {
             <form onSubmit={(e)=>{submitForm(e)}}>
                 <div className="input-wrapper">
                     <label htmlFor="email">Email</label>
-                    <input type="text" id="email" onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type="text" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" onChange={(e) => { setPassword(e.target.value)}} />
+                    <input type="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value)}} />
                 </div>
                 <div className="input-remember">
-                    <input type="checkbox" id="remember-me" onChange={(e) => { setRememberMe(e.target.checked) }}/>
+                    <input type="checkbox" id="remember-me" checked={rememberMe} onChange={(e) => { setRememberMe(e.target.checked) }}/>
                     <label htmlFor="remember-me">Remember me</label>
                 </div>
                 <button className="sign-in-button" type='submit'>Sign In</button>
